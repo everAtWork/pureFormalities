1c2c55b7c7a2959fb0d2379a20099639ceb49926
wladyslawko <59497788+everAtWork@users.noreply.github.com>
2021-04-04 16:58:41 +0300

    got the validation done for the profileForm (1/3) yup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import "./styles.css";
+import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import SignupForm from "./components/SignupForm/SignupForm";
+import "./styles.scss";
 
 export default function App() {
   return (
     <Router>
       <div className="App">
-        <h2>title</h2>
+        <h2>hey</h2>
         <SignupForm />
       </div>
     </Router>
